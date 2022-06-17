df4bb8594d97d1eb9c7fe01a57e9f6fa373db99b
白酒 <tjhao157@gmail.com>
2022-06-17 10:23:56 +0400

    update package.json

---
+++ dist/deckgl-particle-layer.d.ts
@@ -0,0 +1,51 @@
+import { LineLayer } from '@deck.gl/layers';
+import { LineLayerProps } from '@deck.gl/layers';
+import { UpdateStateInfo } from '@deck.gl/core/lib/layer';
+
+/**
+ * Bbox
+ * @public
+ */
+export declare type Bbox = [number, number, number, number];
+
+/**
+ * ParticleLayer
+ * @public
+ */
+export declare class ParticleLayer<D = any, ExtraPropsT = any> extends LineLayer<D, ExtraPropsT & ParticleLayerProps<D>> {
+    getShaders(): any;
+    initializeState(): void;
+    updateState({ props, oldProps, changeFlags }: UpdateStateInfo<ParticleLayerProps<D>>): void;
+    finalizeState(): void;
+    draw({ uniforms }: {
+        uniforms: any;
+    }): void;
+    _setupTransformFeedback(): void;
+    _runTransformFeedback(): void;
+    _deleteTransformFeedback(): void;
+    step(): void;
+    clear(): void;
+    getColorRamp(colors: Record<number, string>): string;
+}
+
+/**
+ * ParticleLayerProps
+ * @public
+ */
+export declare type ParticleLayerProps<D> = LineLayerProps<D> & {
+    image: string;
+    bounds?: Bbox;
+    numParticles?: number;
+    maxAge?: number;
+    speedFactor?: number;
+    colors?: Record<number, string>;
+    width?: number;
+    uWindMin?: number;
+    uWindMax?: number;
+    vWindMin?: number;
+    vWindMax?: number;
+    animate?: number;
+    boundsClip?: boolean;
+};
+
+export { }
